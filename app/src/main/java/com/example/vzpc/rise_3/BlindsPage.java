a40d27f6a03a1a4e4496a677d02640e37fc26cec
KeinZeit <kenny.yau@tufts.edu>
2017-05-02 02:25:47 +0800

    added moving blinds mechanism

--- app/src/main/java/com/example/vzpc/rise_3/BlindsPage.java
+++ app/src/main/java/com/example/vzpc/rise_3/BlindsPage.java
@@ -3,11 +3,16 @@ package com.example.vzpc.rise_3;
 
 
 import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class BlindsPage extends Fragment {
 
@@ -24,7 +29,35 @@ public class BlindsPage extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blinds_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_blinds_page, container, false);
+
+        SeekBar mSeekBar = (SeekBar) view.findViewById(R.id.seekBar);
+        final ImageView blindsView = (ImageView) view.findViewById(R.id.blindsView);
+
+
+        SeekBar.OnSeekBarChangeListener mSeekBarListener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ViewGroup.LayoutParams params = blindsView.getLayoutParams();
+                params.height = progress;
+                blindsView.setLayoutParams(params);
+                Log.d("poop", String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+
+        mSeekBar.setOnSeekBarChangeListener(mSeekBarListener);
+        return view;
+
     }
 
     @Override
